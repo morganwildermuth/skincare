48290803c377f7bfce50bd81ea482067a4685ad6
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-08-30 19:47:14 -0700

    root set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SkincareApp::Application.routes.draw do
+
+  root 'search#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
