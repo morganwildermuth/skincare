b53b45ceeb347a052e922b5cbf5a9d606442c1f9
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-08-30 18:46:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkincareApp::Application.config.secret_key_base = '89b745fbbd32fb482a2e73daf313c787009bb30229f71c81ff6590e6b9dc39e8e0a8fe6d265566cab6d3f3174df4a5652a2227625a0a16dbad33450be418dd0a'
