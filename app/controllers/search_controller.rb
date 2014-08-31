9f643eddcd36ccf990e847b39bcdf1c69e956f57
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-08-30 20:14:22 -0700

    removed unnecessary requires

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    require 'rubygems'
-    require 'mechanize'
     mechanize = Mechanize.new
     page = mechanize.get('http://stackoverflow.com/')
 
