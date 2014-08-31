0cf2d725ebb149cd2c8edcf65784be270fae5878
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-08-30 20:06:38 -0700

    mechanize functional

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
-  def index
 
+  def index
+    require 'rubygems'
+    require 'mechanize'
+    mechanize = Mechanize.new
+    page = mechanize.get('http://stackoverflow.com/')
+
+    @title = page.title
   end
 end
