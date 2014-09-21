30b6ce87ec272300c299d8423817ab862fdeccb4
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-09-20 22:21:24 -0700

    search tearms yielded and listed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,10 @@ class SearchController < ApplicationController
 
   def index
     mechanize = Mechanize.new
-    searchString = params["searchTerm"].gsub!(" ", "+")
-    page = mechanize.get('http://cosdna.com/eng/product.php?q=' + searchString)
-    @links = page.links_with(:href => %r{^cosmetic_})
+    if params["searchTerm"]
+      searchString = params["searchTerm"].gsub!(" ", "+")
+      page = mechanize.get('http://cosdna.com/eng/product.php?q=' + searchString)
+      @links = page.links_with(:href => %r{^cosmetic_})
+    end
   end
 end
