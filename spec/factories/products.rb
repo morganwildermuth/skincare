794c76de1b727679ea2561ea72b47185cb80d9b1
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-09-30 01:59:33 -0700

    updated factory girl to include type cleanser as default for product"

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     name {Faker::Lorem.word.capitalize + " " + Faker::Lorem.word.capitalize}
     description {Faker::Lorem.sentence}
     image_cosdna {Faker::Lorem.word}
+    type {"cleanser"}
 
   end
 end
